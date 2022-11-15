74cf80aaaa0b179aa37b383953eb993a183f8cf1
Michał Zastocki <kontakt@michalzastocki.pl>
2022-11-15 22:44:20 +0100

    add AccountService and method to register new users with hashed password

---
+++ RestaurantAPI3/Services/AccountService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RestaurantAPI3.Entites;
+using RestaurantAPI3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI3.Services
+{
+    public interface IAccountService
+    {
+        void RegisterUser(RegisterUserDto dto);
+    }
+    public class AccountService : IAccountService
+    {
+        private readonly RestaurantDBContext _dbContext;
+        private readonly IPasswordHasher<User> _passwordHasher;
+
+        public AccountService(RestaurantDBContext context, IPasswordHasher<User> passwordHasher)
+        {
+            _dbContext = context;
+            _passwordHasher = passwordHasher;
+        }
+        public void RegisterUser(RegisterUserDto dto)
+        {
+            var newUser = new User()
+            {
+                Email = dto.Email,
+                DateOfBirth = dto.DateOfDirth,
+                Nationality = dto.Nationality,
+                RoleId = dto.RoleId
+            };
+
+            var hashedPassword = _passwordHasher.HashPassword(newUser, dto.Password);
+            newUser.PasswordHash = hashedPassword;
+
+            _dbContext.Users.Add(newUser);
+            _dbContext.SaveChanges();
+
+        }
+    }
+}
--- RestaurantAPI3/Startup.cs
+++ RestaurantAPI3/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -37,6 +38,8 @@ namespace RestaurantAPI3
             services.AddScoped<RequestTimeMiddleware>();
             services.AddSwaggerGen();
             services.AddScoped<IDishService, DishService>();
+            services.AddScoped<IAccountService, AccountService>();
+            services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
