06352ac642ddacf6ded60f611063930c4a6ba0e3
Michał Zastocki <kontakt@michalzastocki.pl>
2022-11-29 23:08:04 +0100

    Add CORS and FileController with caching

--- RestaurantAPI3/Startup.cs
+++ RestaurantAPI3/Startup.cs
@@ -83,11 +83,23 @@ namespace RestaurantAPI3
             services.AddScoped<IUserContextService, UserContextService>();
             services.AddHttpContextAccessor();
             services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("FrontEndClient", builder =>
+                    builder.AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .WithOrigins(Configuration["AllowedOrigins"])
+                    
+                );
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, RestaurantSeeder seeder)
         {
+            app.UseResponseCaching();
+            app.UseStaticFiles();
+            app.UseCors("FrontEndClient");
             seeder.Seed();
             if (env.IsDevelopment())
             {
